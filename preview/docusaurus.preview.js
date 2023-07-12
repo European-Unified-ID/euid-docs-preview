6c1a06f63efeb96285b208ee333ee6187c8c4cba
UnifiedID2 <>
2023-07-12 05:41:16 +0000

    Update from https://github.com/IABTechLab/uid2docs/commit/70b865eb76973e5ae2ee7dfeec1a87b25fedbfec

--- preview/docusaurus.preview.js
+++ preview/docusaurus.preview.js
@@ -1,20 +1,22 @@
-import original from './docusaurus.original';
+import * as original from './docusaurus.original.js';
 
 /*
  * N.B. This file is used for the preview site. 
  * `docusaurus.config.js` is renamed to `docusaurus.original.js`, this file is renamed to `docusaurus.config.js`, and
  * then this file is included (which will import the original file and update the settings for the preview site).
 */
-const config = {
-  ...original,
-  url: "https://european-unified-id.github.io/",
-  baseUrl: "/euid-docs-preview/",
-  organizationName: "European-Unified-ID", // Usually your GitHub org/user name.
-  projectName: "euid-docs-preview", // Usually your repo name.
+
+config = {
+  ...original.default,
+  url: "https://unifiedid2.github.io/",
+  baseUrl: "/uid2-docs-preview/",
+  organizationName: "UnifiedID2", // Usually your GitHub org/user name.
+  projectName: "uid2-docs-preview", // Usually your repo name.
   themeConfig: {
     ...original.themeConfig,
     algolia: undefined
   }
 };
 
-export default config;
+
+module.exports = config;
