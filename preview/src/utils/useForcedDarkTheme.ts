6950c087820f6fc606f6b5daec3f263b926a0e92
UnifiedID2 <>
2024-05-01 17:26:06 +0000

    Update from https://github.com/IABTechLab/uid2docs/commit/0f477e77d598a76c7eacbc52de612000072fa4db

--- preview/src/utils/useForcedDarkTheme.ts
+++ preview/src/utils/useForcedDarkTheme.ts
@@ -1,14 +1,13 @@
 import * as React from "react";
-import { useColorMode, ColorMode } from "@docusaurus/theme-common";
+import { useColorMode } from "@docusaurus/theme-common";
 
 export function useForcedDarkTheme() {
   const { setColorMode, colorMode } = useColorMode();
   //quick fix for updating color mode on page load breaking after theme upgrade
   React.useEffect(() => {
-    const originalTheme =
-      (localStorage.getItem("theme") as ColorMode) ?? colorMode;
+    const originalTheme = colorMode;
 
-    //@ts-expect-error - The second parameter exists, it's just not on the type :(
+    // The second parameter exists, it's just not on the type :(
     setColorMode("dark", { persist: false });
 
     return () => {
