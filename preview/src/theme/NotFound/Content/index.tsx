ff8d42c0c51e9d12e44708a149e017a6d261aba4
UnifiedID2 <>
2025-02-24 15:47:13 +0000

    Update from https://github.com/IABTechLab/uid2docs/commit/c9d902a1e3bc9f0705fcab32da506c452118cb6e

--- preview/src/theme/NotFound/Content/index.tsx
+++ preview/src/theme/NotFound/Content/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import Translate from "@docusaurus/Translate";
 import type { Props } from "@theme/NotFound/Content";
 import Heading from "@theme/Heading";
 import Link from "@docusaurus/Link";
@@ -10,22 +11,56 @@ export default function NotFoundContent({ className }: Props): JSX.Element {
       <div className="row">
         <div className="col col--6 col--offset-3">
           <Heading as="h1" className="hero__title">
-            Page Not Found
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page"
+            >
+              Page Not Found
+            </Translate>
           </Heading>
           <p>
-            We couldn't find the page you were looking for. The page might have
-            been moved, or there was an error in the link.
+            <Translate
+              id="theme.NotFound.p1"
+              description="The first paragraph of the 404 page"
+            >
+              We couldn't find the page you were looking for. The page might
+              have been moved, or there was an error in the link.
+            </Translate>
           </p>
-          <p>Here are some things you can do:</p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page"
+            >
+              Here are some things you can do:
+            </Translate>
+          </p>
           <ul>
-            <li>Go back and retry the link.</li>
             <li>
-              Search for the document by its title or keywords from any EUID
-              page.
+              <Translate
+                id="theme.NotFound.ListItem1"
+                description="The first list item of the 404 page"
+              >
+                Go back and retry the link.
+              </Translate>
+            </li>
+            <li>
+              <Translate
+                id="theme.NotFound.ListItem2"
+                description="The second list item of the 404 page"
+              >
+                Search for the document by its title or keywords from any UID2
+                page.
+              </Translate>
             </li>
             <li>
               <Link to="/docs/intro">
-                Visit the EUID documentation home page.
+                <Translate
+                  id="theme.NotFound.ListItem3"
+                  description="The third list item of the 404 page"
+                >
+                  Visit the UID2 documentation home page.
+                </Translate>
               </Link>
             </li>
           </ul>
