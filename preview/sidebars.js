9ddf958c2b387d6c95797e2af3da25dadae59107
UnifiedID2 <>
2025-07-08 05:59:40 +0000

    Update from https://github.com/IABTechLab/uid2docs/commit/029854d5b27ae25cad10b41e70b5ac4697e96529

--- preview/sidebars.js
+++ preview/sidebars.js
@@ -328,7 +328,7 @@ const fullSidebar = [
 
     {
       type: 'category',
-      label: 'Endpoints (v2)',
+      label: 'Endpoints',
       link: {
         type: 'doc',
         id: 'endpoints/summary-endpoints',
@@ -339,7 +339,18 @@ const fullSidebar = [
         'endpoints/post-token-validate',
         'endpoints/post-token-refresh',
         'endpoints/post-identity-buckets',
-        'endpoints/post-identity-map',
+        {
+          type: 'category',
+          label: 'POST /identity/map',
+          link: {
+            type: 'doc',
+            id: 'endpoints/post-identity-map',
+          },
+          collapsed: true,
+          items: [
+            'endpoints/post-identity-map-v2',
+          ],
+        },
         'endpoints/post-optout-status',
       ],
     },
@@ -393,7 +404,6 @@ const fullSidebar = [
         'ref-info/ref-how-uid-is-created',
         'ref-info/ref-server-side-token-generation',
         'ref-info/ref-integration-sso-providers',
-        'ref-info/deprecation-schedule',
       ],
     },
 
@@ -419,6 +429,7 @@ const sidebars = {
     'guides/dsp-guide',
     'endpoints/post-identity-buckets',
     'endpoints/post-identity-map',
+    'endpoints/post-identity-map-v2',
     'endpoints/post-optout-status'
   ),
 
