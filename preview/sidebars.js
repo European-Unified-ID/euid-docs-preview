e12f0b2d4a7254840c3e801f726b2018104ab5a4
UnifiedID2 <>
2025-07-09 18:47:40 +0000

    Update from https://github.com/IABTechLab/uid2docs/commit/5cd50518b4f74b4901f3dac96daecef635808086

--- preview/sidebars.js
+++ preview/sidebars.js
@@ -339,7 +339,18 @@ const fullSidebar = [
         'endpoints/post-token-validate',
         'endpoints/post-token-refresh',
         'endpoints/post-identity-buckets',
-        'endpoints/post-identity-map',
+        {
+          type: 'category',
+          label: 'POST /identity/map',
+          link: {
+            type: 'doc',
+            id: 'endpoints/post-identity-map',
+          },
+          collapsed: true,
+          items: [
+            'endpoints/post-identity-map-v2',
+          ],
+        },
         'endpoints/post-optout-status',
       ],
     },
@@ -419,6 +430,7 @@ const sidebars = {
     'guides/dsp-guide',
     'endpoints/post-identity-buckets',
     'endpoints/post-identity-map',
+    'endpoints/post-identity-map-v2',
     'endpoints/post-optout-status'
   ),
 
